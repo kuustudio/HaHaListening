8c839b1ffeca99e16cd2b98e1f46d28c81bcf5d4
王雁 <wangyan@sogou-inc.com>
2014-02-17 22:53:37 +0800

    start of LearningBook Project

---
+++ ListeningBook/XXCNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XXCNAppDelegate.h
+//  ListeningBook
+//
+//  Created by YY on 14-2-17.
+//  Copyright (c) 2014年 小小菜鸟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XXCNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ListeningBook/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ListeningBook
+//
+//  Created by YY on 14-2-17.
+//  Copyright (c) 2014年 小小菜鸟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XXCNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XXCNAppDelegate class]));
+    }
+}
